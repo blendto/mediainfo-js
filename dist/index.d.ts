38dcc512322367c0238502424eafaff48a569f67
deepak <deepak@blend.to>
2025-04-09 12:22:10 +0530

    include types

---
+++ dist/index.d.ts
@@ -0,0 +1,14 @@
+import { Stream } from 'stream';
+
+type MediaInfoInput = URL | string | Buffer | Stream;
+type MediaInfoData = any;
+declare class MediaInfo {
+    private lib;
+    instantiateLib(): Promise<void>;
+    getInfo(input: MediaInfoInput): Promise<MediaInfoData>;
+    private static normalizeInput;
+    private static getDataStream;
+    private getMediaInfoData;
+}
+
+export { MediaInfo, type MediaInfoData, type MediaInfoInput };
